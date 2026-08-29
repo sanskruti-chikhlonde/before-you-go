47d1fbe75ee0761277a0ec242a266c466a9155b2
sanskruti-chikhlonde <sanskruti0804@gmail.com>
2026-08-29 23:09:14 +0530

    select form and theme elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const tripForm = document.getElementById("trip-form");
+const destinationInput = document.getElementById("destination");
+const daysInput = document.getElementById("days");
+
+
+/* THEME TOGGLE */
+const themeToggle = document.getElementById("theme-toggle");
